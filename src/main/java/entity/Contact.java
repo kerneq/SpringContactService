ef2c12826faa69ecf89915524f1ecd078dcbfb7a
kerneq <stasik-0@yandex.ru>
2017-08-15 01:49:11 +0300

    add entity class Contact

--- src/main/java/entity/Contact.java
+++ src/main/java/entity/Contact.java
@@ -1,7 +1,112 @@
 package entity;
 
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+
 /**
  * Created by iters on 8/15/17.
  */
+
+@Entity
+@Table(name="contact")
 public class Contact {
+    private Long id;
+    private int version;
+    private String firstName;
+    private String lastName;
+    private DateTime birthDate;
+    private String description;
+    private byte[] photo;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Version
+    @Column(name = "VERSION")
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @Column(name="FIRST_NAME")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name="LAST_NAME")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "BIRTH_DATE")
+    @Type(type="org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    @DateTimeFormat(iso= DateTimeFormat.ISO.DATE)
+    public DateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(DateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Column(name="DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Basic(fetch=FetchType.LAZY)
+    @Lob
+    @Column(name = "PHOTO")
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public String getBirthDateSrtring() {
+        String birthDateString = "";
+        if (birthDate != null) {
+            birthDateString = org.joda.time.format.DateTimeFormat
+                    .forPattern("yyyy-MM-dd")
+                    .print(birthDate);
+        }
+
+        return birthDateString;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact - Id: " + id + ", First name: " + firstName
+                + ", Last name: " + lastName + ", Birthday: " + birthDate
+                + ", Description: " + description;
+    }
+
 }
