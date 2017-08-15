aab61fb0b1cc725e95680049489294b21db0f2e2
kerneq <stasik-0@yandex.ru>
2017-08-15 22:50:58 +0300

    fix problem with PackagesToScan in servlet-context

--- src/main/java/service/ContactServiceImpl.java
+++ src/main/java/service/ContactServiceImpl.java
@@ -6,8 +6,8 @@ import interfaces.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import repository.ContactRepository;
-import javax.transaction.Transactional;
 import java.util.List;
 
 /**
